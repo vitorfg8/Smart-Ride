da1dfbf1cff458b53087aef0e9396c75c9e7c6e6
Vítor Ferreira <vitorfer237@gmail.com>
2024-12-06 12:10:12 -0300

    add RequestRideScreen layout

--- app/src/main/java/com/vitorfg8/smartride/MainActivity.kt
+++ app/src/main/java/com/vitorfg8/smartride/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.vitorfg8.smartride.ui.presentation.RequestRideScreen
 import com.vitorfg8.smartride.ui.theme.SmartRideTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SmartRideTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                RequestRideScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartRideTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/vitorfg8/smartride/ui/presentation/RequestRideScreen.kt
@@ -0,0 +1,68 @@
+package com.vitorfg8.smartride.ui.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vitorfg8.smartride.R
+import com.vitorfg8.smartride.ui.theme.SmartRideTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+@Composable
+fun RequestRideScreen(modifier: Modifier = Modifier) {
+    Scaffold(modifier = modifier) { padding ->
+        Column(
+            modifier = Modifier.padding(padding).fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 24.dp, end = 24.dp, top = 16.dp, bottom = 8.dp),
+                value = stringResource(R.string.enter_your_id),
+                onValueChange = { newValue -> /* ... */ })
+            TextField(modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 24.dp, end = 24.dp, top = 8.dp, bottom = 8.dp),
+                value = stringResource(R.string.enter_your_starting_location),
+                onValueChange = { newValue -> /* ... */ })
+            TextField(modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 24.dp, end = 24.dp, top = 8.dp, bottom = 16.dp),
+                value = stringResource(R.string.enter_your_arrival_location),
+                onValueChange = { newValue -> /* ... */ })
+            Button(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp), onClick = {}) {
+                Text(stringResource(R.string.estimate_the_value_of_the_trip))
+            }
+
+            CircularProgressIndicator(
+                modifier = Modifier.size(48.dp),
+                strokeWidth = 4.dp
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun RequestRideScreenPreview() {
+    SmartRideTheme {
+        RequestRideScreen()
+    }
+}
