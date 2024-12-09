d3fdec1c0a716199236e5d9ab1885bac6f95c03d
Vítor Ferreira <vitorfer237@gmail.com>
2024-12-09 16:07:39 -0300

    update RideOptionsScreen.kt layout

--- app/src/main/java/com/vitorfg8/smartride/ui/rideoptions/RideOptionsScreen.kt
+++ app/src/main/java/com/vitorfg8/smartride/ui/rideoptions/RideOptionsScreen.kt
@@ -1,12 +1,115 @@
 package com.vitorfg8.smartride.ui.rideoptions
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Polyline
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.vitorfg8.smartride.R
 import com.vitorfg8.smartride.ui.theme.SmartRideTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RideOptionsScreen(modifier: Modifier = Modifier) {
+    Scaffold(modifier = modifier, topBar = {
+        TopAppBar(title = { Text(stringResource(R.string.ride_options)) }, navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.go_back)
+                )
+            }
+        })
+    }) { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+        ) {
+            item {
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    text = stringResource(R.string.estimated_route),
+                    style = MaterialTheme.typography.titleMedium,
+                )
+                Map()
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    style = MaterialTheme.typography.titleMedium,
+                    text = stringResource(R.string.driver_options)
+                )
+            }
+            items(3) {
+                //OptionsItem()
+            }
+        }
+    }
+}
+
+@Composable
+private fun Map(modifier: Modifier = Modifier) {
+    val cameraPosition = LatLng(-23.5215624, -46.763286699999995)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(cameraPosition, 15f)
+    }
+
+    var uiSettings by remember {
+        mutableStateOf(MapUiSettings(zoomControlsEnabled = true))
+    }
+    var properties by remember {
+        mutableStateOf(MapProperties(mapType = MapType.NORMAL))
+    }
+
+    val routeCoordinates = listOf(
+        LatLng(-23.5215624, -46.763286699999995),
+        LatLng(-23.5615351, -46.6562816),
+    )
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(450.dp)
+            .padding(16.dp)
+    ) {
+
+        GoogleMap(
+            cameraPositionState = cameraPositionState,
+            properties = properties,
+            uiSettings = uiSettings
+        ) {
+            Polyline(
+                points = routeCoordinates, clickable = true, color = Color.Blue, width = 5f
+            )
+        }
+    }
 
 }
 
