c545eb82f4706f71806af0c561aa746847fad827
Vítor Ferreira <vitorfer237@gmail.com>
2024-12-09 17:41:57 -0300

    add gradle secrets plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
