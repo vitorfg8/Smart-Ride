f3ce90ebd0ed941ec1a45908fabe4512dba51a06
Vítor Ferreira <vitorfer237@gmail.com>
2024-12-10 10:37:36 -0300

    add RideHistoryScreen.kt

---
+++ app/src/main/java/com/vitorfg8/smartride/domain/model/ridehistory/RideHistoryScreen.kt
@@ -0,0 +1,19 @@
+package com.vitorfg8.smartride.domain.model.ridehistory
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vitorfg8.smartride.ui.theme.SmartRideTheme
+
+@Composable
+fun RideHistoryScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun RideHistoryScreenPreview() {
+    SmartRideTheme {
+        RideHistoryScreen()
+    }
+}
